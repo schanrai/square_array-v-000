3e9d3856ad05ad516468c10f3bafae36dcc521cd
Sushi Chanrai <sushi@pathworks.co>
2018-06-27 21:12:13 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,15 @@
-=begin
+
 def square_array(numbers)
   new_arr = []
   numbers.each {|x| new_arr << x**2}
   return new_arr
 end
-=end
+
 
   #in code below puts prints out 1,4,9, return just does 1,2,3
   #numbers.each { |x| puts x**2 }
-  
+=begin 
 def square_array(numbers)
   numbers.map {|x| x**2}
-end  
+end 
+=end
